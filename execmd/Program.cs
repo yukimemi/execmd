718c5d504cf941f93219974526ea1e423465c67b
yukimemi <yukimemi@gmail.com>
2020-03-19 22:11:54 +0900

    :+1: Initial commit.

---
+++ execmd/Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace execmd
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var cmdPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+      var cmdDir = System.IO.Path.GetDirectoryName(cmdPath);
+      var cmdName = System.IO.Path.GetFileNameWithoutExtension(cmdPath);
+      var execmd = System.IO.Path.Combine(cmdDir, cmdName + ".cmd");
+      var proc = new System.Diagnostics.Process();
+      proc.StartInfo.FileName = execmd;
+      proc.StartInfo.Arguments = string.Join(" ", args);
+
+      proc.Start();
+
+      proc.WaitForExit();
+
+      System.Environment.Exit(proc.ExitCode);
+
+    }
+  }
+}
